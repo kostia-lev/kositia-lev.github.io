a78ce987ae0b5b19c9436fd6f848d3736205ae56
Konstantinv Vahrushev <konstantinv@spotoption.com>
2017-04-23 21:33:02 +0300

    added fonts and one margin

--- js/bundle.js
+++ js/bundle.js
@@ -11,14 +11,6 @@ angular
 (function(angular){
 'use strict';
 angular
-  .module('components', [
-    'components.contact',
-    'components.search'
-  ]);
-})(window.angular);
-(function(angular){
-'use strict';
-angular
   .module('common', [
     'ui.router'
   ]);
@@ -26,8 +18,8 @@ angular
 (function(angular){
 'use strict';
 angular
-  .module('components.contact', [
-    'ui.router'
+  .module('components', [
+    'components.search'
   ]);
 })(window.angular);
 (function(angular){
@@ -260,7 +252,7 @@ const data = [
 'use strict';
 angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('./root.html','<div class="root"><nav><searchinput st="parent.searchText"></searchinput></nav><main><searchresults fetchedarr="parent.fetchedData" albumstotal="parent.albumsTotal" artiststotal="parent.artistsTotal"></searchresults><searchpopup showmodal="parent.showModal" modaltitle="parent.modalTitle" modaltype="parent.modalType" modaldata="parent.modalData" modalpicture="parent.modalPicture"></searchpopup></main></div>');
 $templateCache.put('./searchinput.html','<div id="search-container"><input type="text" placeholder="Search for artist or album title" ng-model="$ctrl.st" ng-keypress="$event.which === 13 && $ctrl.onclick()"><div id="search-button" ng-click="$ctrl.onclick();"><span>SEARCH</span></div></div>');
-$templateCache.put('./searchitem.html','<div class="searchitem {{$ctrl.itemtype}}"><div class="itempic cell A1"><div class="alBg" style="background-image:url(\'{{$ctrl.itemimgs[1][\'url\']}}\')"></div>f<div class="itempicContainer txt" ng-click="$emit(\'viewitem\', {type: $ctrl.itemtype, id: $ctrl.itemid});"><img ng-src="{{$ctrl.itemtype==\'album\'? \'/img/album-icon@2x.png\':\'/img/artist-icon@2x.png\'}}"> {{$ctrl.itemtype==\'album\'? \'View tracks\':\'View albums\'}}</div></div><div class="itemnamecontainer"><div class="itemname" ng-transclude="name"></div></div></div>');
+$templateCache.put('./searchitem.html','<div class="searchitem {{$ctrl.itemtype}}"><div class="itempic cell A1"><div class="alBg" style="background-image:url(\'{{$ctrl.itemimgs[1][\'url\']}}\')"></div><div class="itempicContainer txt" ng-click="$emit(\'viewitem\', {type: $ctrl.itemtype, id: $ctrl.itemid});"><img ng-src="{{$ctrl.itemtype==\'album\'? \'/img/album-icon@2x.png\':\'/img/artist-icon@2x.png\'}}"> {{$ctrl.itemtype==\'album\'? \'View tracks\':\'View albums\'}}</div></div><div class="itemnamecontainer"><div class="itemname" ng-transclude="name"></div></div></div>');
 $templateCache.put('./searchpopup.html','<div id="modal-container" ng-show="$ctrl.showmodal"><div id="modal-dimmed-part"></div><figure id="modal-data-container"><div class="firstRow" style="background-image: url(\'{{$ctrl.modalpicture.url}}\')"><img id="modalCross" class="modalCross" src="/img/Cross@2x.png" height="20" width="20" ng-click="$ctrl.showmodal = false"> <img id="modalCrossBlack" class="modalCross" src="/img/Cross Dark@2x.png" height="20" width="20" ng-click="$ctrl.showmodal = false"> <span id="albumTitle"><span class="trackson">{{$ctrl.modaltype == \'artist\'? \'Albums by\':\'Tracks on\'}}</span><br>{{$ctrl.modaltitle}}</span></div><div class="secondRow"><div id="captionContainer"><span>{{$ctrl.modaltype == \'artist\'? \'Albums\':\'Tracks\'}}</span></div><div id="albumRowsContainer"><div class="albumRow" ng-repeat="item in $ctrl.modaldata track by $index"><div ng-show="$ctrl.modaltype==\'artist\'" class="albumImgContainer" style="background-image:url(\'{{item.images[2].url}}\')"></div><div class="albumDataContainer"><div class="albumNameContainer">{{item.name}}</div><div class="albumYearContainer">{{$ctrl.modaltype == \'artist\'? \'-\':$ctrl.formatDate(item.duration_ms)|date:\'mm:ss\'}}</div></div></div></div></div></figure></div>');
 $templateCache.put('./searchresults.html','<div id="willappear" ng-if="!$ctrl.fetchedarr.length"><div id="pusher"><img src="img/Magnify@2x.png"><br><span>Your results will appear here</span></div></div><searchitem itemtype="item.type" itemimgs="item.images" itemid="item.id" ng-repeat="item in $ctrl.fetchedarr track by $index"><h5>{{item.name}}</h5><img ng-src="{{item.images[1][\'url\']}}" width="263" height="263"></searchitem><div id="showmemore-button" ng-click="$ctrl.showmemore();" ng-show="!!$ctrl.fetchedarr.length && ($ctrl.fetchedarr.length<($ctrl.artiststotal + $ctrl.artiststotal))"><span>SHOW ME MORE</span></div>');}]);})(window.angular);
 //# sourceMappingURL=bundle.js.map
